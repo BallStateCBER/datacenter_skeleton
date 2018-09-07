4314dfcb974e77c17a8901188b27d3c9c32b0145
Graham Watson <graham@phantomwatson.com>
2018-09-07 19:12:50 -0400

    Update how RoutingMiddleware is loaded

--- src/Application.php
+++ src/Application.php
@@ -61,7 +61,7 @@ class Application extends BaseApplication
             ->add(AssetMiddleware::class)
 
             // Apply routing
-            ->add(RoutingMiddleware::class);
+            ->add(new RoutingMiddleware($this));
 
         return $middlewareQueue;
     }
